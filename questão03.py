463ec31922a5ceba763ca8d7181fabdeec8cc4a8
edimundo silva <edimundobraga@gmail.com>
2024-09-05 15:02:11 -0300

    novo commit

---
+++ questão03.py
@@ -0,0 +1,18 @@
+
+quantidade_notas = int(input("Digite a quantidade de notas que deseja calcular: "))
+
+soma_notas = 0
+
+for i in range(1, quantidade_notas + 1):
+    nota = float(input(f"Digite a nota {i}: "))
+    soma_notas += nota
+
+
+media = soma_notas / quantidade_notas
+
+
+print(f"\nMédia final: {media:.2f}")
+if media >= 7:
+    print("Resultado: Aprovado")
+else:
+    print("Resultado: Reprovado")
